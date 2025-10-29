ead36a275fb3ba11fe80a50c1f1686b01dfe20a6
Jaimie Bowen <jraebowen@gmail.com>
2025-10-29 15:38:42 -0700

    add the about section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,12 +5,14 @@ import "./App.css";
 
 //component imports
 import Header from "../Header/Header";
+import Main from "../Main/Main";
 
 function App() {
   return (
     <div className="page">
       <div className="page__content">
         <Header></Header>
+        <Main></Main>
       </div>
     </div>
   );
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,9 @@
+import "./Main.css";
+
+import About from "../About/About";
+
+function Main() {
+  return <About></About>;
+}
+
+export default Main;
