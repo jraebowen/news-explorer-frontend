639234b71101560baa34591f5421b75b7a91a1fe
Jaimie Bowen <jraebowen@gmail.com>
2025-10-30 16:30:25 -0700

    update newscardlist to render hard coded cards

--- src/components/NewsCardList/NewsCardList.jsx
+++ src/components/NewsCardList/NewsCardList.jsx
@@ -1,12 +1,15 @@
 import "./NewsCardList.css";
 
 import NewsCards from "../NewsCards/NewsCards";
+import { searchResults } from "../../utils/constants";
 
 function NewsCardList() {
   return (
     <section className="news">
       <ul className="news-card-list">
-        <NewsCards></NewsCards>
+        {searchResults.map((item) => {
+          return <NewsCards item={item}></NewsCards>;
+        })}
       </ul>
     </section>
   );
--- src/components/NewsCards/NewsCards.jsx
+++ src/components/NewsCards/NewsCards.jsx
@@ -1,24 +1,31 @@
 import "./NewsCards.css";
 
-import placeholder from "../../assets/placeholder.jpg";
+function NewsCards({ item }) {
+  const date = item.publishedAt;
+  const dateObject = new Date(date);
+  const updatedDate = dateObject.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
-function NewsCards() {
+  const capitalSrc = item.source.toUpperCase();
+
   return (
     <li className="news-card">
       <div className="news-card__contents">
-        <img src={placeholder} alt="" className="news-card__image" />
+        <img
+          src={item.urlToImage}
+          alt={item.title}
+          className="news-card__image"
+        />
         <div className="news-card__text">
-          <p className="news-card__text-date">November 4, 2020</p>
-          <p className="news-card__text-title">
-            Everyone Needs a Special 'Sit Spot' in Nature
-          </p>
-          <p className="news-card__text-description">
-            Ever since I read Richard Louv's influential book, "Last Child in
-            the Woods," the idea of having a special "sit spot" has stuck with
-            me. This advice, which Louv attributes to nature educator Jon Young,
-            is for both adults and children to find...
-          </p>
-          <p className="news-card__text-source">TREEHUGGER</p>
+          <p className="news-card__text-date">{updatedDate}</p>
+          <p className="news-card__text-title">{item.title}</p>
+          <div className="news-card__positioning-container">
+            <p className="news-card__text-description">{item.description}</p>
+            <p className="news-card__text-source">{capitalSrc}</p>
+          </div>
         </div>
       </div>
     </li>
