8d94fa77673d32801e38f0ba5413aa7028e5ced1
Jaimie Bowen <jraebowen@gmail.com>
2025-11-10 20:17:11 -0800

    update rep name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/news-explorer-frontend/",
+  base: "/news-explorer/",
   server: {
     port: 3000,
   },
