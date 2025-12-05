f317d125045f0da5bbc11ca3730507a6bd6919cd
Jaimie Bowen <jraebowen@gmail.com>
2025-12-05 15:17:12 -0800

    update base url in vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/final-project-frontend/",
+  base: "/",
   server: {
     port: 3000,
   },
